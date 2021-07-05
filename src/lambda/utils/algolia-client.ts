9086f1824b78acb8706498eda6fe9e57a682d6fc
kenticohero <maartenv@kentico.com>
2021-07-05 15:53:51 +0200

    try fix type filter

--- src/lambda/utils/algolia-client.ts
+++ src/lambda/utils/algolia-client.ts
@@ -32,10 +32,10 @@ class AlgoliaClient {
   }
 
   // returns the indexed content item(s) that include searched content item
-  async searchIndex(searchedType: string, client: string): Promise<SearchableItem[]> {
+  async searchIndex(type: string, client: string): Promise<SearchableItem[]> {
     try {
       const response = await this.index.search<SearchableItem>("", {
-        facetFilters: [`type: ${searchedType}`, `client: ${client}`]
+        facetFilters: [`type: ${type}`, `client: ${client}`]
       });
       return response.hits;
     }
